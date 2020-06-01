2a39193f574f2d03b7c0efb2d9525386ee025f99
PingPingE <ds03023@gmail.com>
2020-06-01 17:29:13 +0900

    이것이 자바다. chapter07

--- chap07/src/exam04/SmartPhone.java
+++ chap07/src/exam04/SmartPhone.java
@@ -7,7 +7,7 @@ public class SmartPhone extends Phone {
 		// TODO Auto-generated constructor stub
 	}
 
-	//자식클래스들은 똑같은 메소드 이름으로 각자의 기능을 구현할 수 있다. 이것이 추상클래스의 장점
+	//자식클래스들은 똑같은 메소드 이름으로 각자의 기능을 구현할 수 있다. 이것이 추상화의 장점
 	@Override
 	public void bellOn() {
 	
